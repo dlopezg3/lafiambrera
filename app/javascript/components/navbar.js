9a3a3af5b749c454be32a438b695feb6d4b5e150
Daniel Lopez <dlopez.10g@gmail.com>
2020-02-18 22:54:07 -0500

    color adjusments

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,10 +3,8 @@ const initUpdateNavbarOnScroll = ()  => {
   if (navbar) {
     window.addEventListener("scroll", () => {
       if (window.oldScroll <= window.scrollY) {
-        console.log("bajando");
         navbar.classList.add("navbar-lewagon-transparent");
       } else {
-        console.log("subiendo");
         navbar.classList.remove("navbar-lewagon-transparent");
       }
       window.oldScroll = window.scrollY;
